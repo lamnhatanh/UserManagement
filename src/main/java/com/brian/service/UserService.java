7ed272b32062975e7b2b74a4e2c8138a0cce4630
peter <blam@gaports.com>
2024-01-19 16:45:39 -0500

    Updated filter, jwt token, repo and user service

---
+++ src/main/java/com/brian/service/UserService.java
@@ -0,0 +1,11 @@
+package com.brian.service;
+
+import com.brian.model.User;
+
+
+public interface UserService {
+
+//    User findUserByUsername(String username);
+//
+//    User findUserByEmail(String email);
+}
